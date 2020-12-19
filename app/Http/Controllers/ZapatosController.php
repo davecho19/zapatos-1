1f40f7cd8d9071e488697b929f714d3052115e59
panchoxsj <panchoxsj@hotmail.com>
2020-12-19 17:51:33 -0500

    5.4. Laravel Routing y Controllers

---
+++ app/Http/Controllers/ZapatosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ZapatosController extends Controller
+{
+    public function hola()
+    {
+     return "Desde receta controller";   
+    }
+        
+}
